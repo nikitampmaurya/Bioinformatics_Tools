916fc55b7bea346e4d08e87ca009b5e0539fbeba
Nikita <132465691+nikitampmaurya@users.noreply.github.com>
2023-10-01 18:00:04 +0530

    Update Biopython.py

--- Biopython.py
+++ Biopython.py
@@ -1,27 +1,27 @@
-#Step1: To install Biopython
+#Exercise 1: To install Biopython
 #pip install biopython 
 
-#Step2: To Check Biopython version
+#Exercise 2: To Check Biopython version
 import Bio
 print("Biopython version:",Bio.__version__)
 
-#Step3: To check the data type of a sequence before and after using library 
+#Exercise 3: To check the data type of a sequence before and after using library 
 s = "ATGC"
 print("my sequence: ",s)
 print("Data type of my sequence before importing library:",type(s),"=",s) 
 
-#Step 4: To check the data type of a sequence after importing library
+#Excercise 4: To check the data type of a sequence after importing library
 from Bio.Seq import Seq
 n = Seq(s) #convert string into sequence
 print("Data type of sequence after importing library:", type(n)) 
 
-#Step 5: To print original sequence, complement and reverse of my sequence
+#Exercise 5: To print original sequence, complement and reverse of my sequence
 print("Original sequence:",n)
 print("complement of the original sequence:",n.complement())
 print("reverse of my original sequence",n.reverse_complement())
-print("length of the the original sequence:",len(n))
+print("Length of the the original sequence:",len(n))
 
-#Step 6: To parse simple fasta file, to get every sequence's id, length and 
+#Exercise 6: To parse simple fasta file, to get every sequence's id, length and 
 from Bio import SeqIO 
 file_path = "C:\\Users\\Nikita Maurya\\OneDrive\\Desktop\\Nikita\\pythonfiles\\ls_orchid.fasta.txt"
 for my_sequence_record in SeqIO.parse(file_path,"fasta"): 
@@ -29,7 +29,7 @@ for my_sequence_record in SeqIO.parse(file_path,"fasta"):
     print(repr(my_sequence_record.seq)) 
     print(len(my_sequence_record)) 
 
-#Step 7: To parse genebank file, to get every sequence's id, sequence and length
+#Exercise 7: To parse genebank file, to get every sequence's id, sequence and length
 
 from Bio import SeqIO
 file_path = "C:\\Users\\Nikita Maurya\\OneDrive\\Desktop\\Nikita\\pythonfiles\\ls_orchid.gbk.txt"
@@ -39,7 +39,7 @@ for my_sequence_record in SeqIO.parse(file_path,"genbank"):
     print(len(my_sequence_record))
 
 """
-Step 8: 
+Exercise 8: 
 i)   to print nucleotide present at x position in the given dna sequence
 ii)  to print first and last nucleotide of the given dna sequence
 iii) to slice the given sequence
@@ -61,7 +61,7 @@ print("Total GC content: ",round(cal_GC_content,2))
 from Bio.SeqUtils import gc_fraction                              #v)To directly print GC content
 print("Using gc function to print gc content: ",gc_fraction(s))
 
-#Step 9: To add two sequence 
+#Exercise 9: To add two sequence 
 
 from Bio.Seq import Seq
 seq1 = Seq("AAT")
@@ -70,3 +70,52 @@ new_seq = seq1 + seq2
 print("The first sequence: ",seq1)
 print("The second sequence: ",seq2)
 print("The concatenated sequence: ",new_seq)
+
+#Excercise 10: To add repeating string in the middle of two sequences
+
+from Bio.Seq import Seq
+list_of_seq = [Seq("ATG"),Seq("CGT")]
+add_repeating_string = Seq("X"*5)
+example = add_repeating_string.join(list_of_seq)
+print(example)  
+
+
+"""
+Exercise 11: To transcibe and translate dna sequnce in biopython
+Important note: 
+- transcription in Biopython 
+- in Biopyhton transcription occurs from complementary strand/coding strand and not template strand
+- because it is easy to convert complementary strand/coding strand to mrna, by just switching T to U
+- complementary strand and coding strand have same sequence but serve different purposes
+"""
+from Bio.Seq import Seq
+template_strand = Seq("TACCGGTAAATC")
+print("The template strand running from 3' end to 5'end:",template_strand)
+complementary_strand = template_strand.complement()
+print("The complement strand running from 5' to 3'end:",complementary_strand)
+mrna = complementary_strand.transcribe() #switching T in coding strand/complement strand to U in mrna
+print("mrna strand from 5' end to 3' end:",mrna)
+print("mrna to dna from 5'end to 3' end:", mrna.back_transcribe()) #swiching U in mrna to T in dna
+
+#To translate protein from rna
+translated_mrna = mrna.translate()
+print("The protein translated from mrna:",translated_mrna)
+#to tranlate protein from coding strand
+tranlated_from_complementary_strand = complementary_strand.translate()
+print("The protein from complementary strand: ",tranlated_from_complementary_strand)
+
+
+"""
+Exercise 12: 
+"""
+from Bio.Seq import MutableSeq
+sequence = MutableSeq("GCCATTG")
+print("Original sequence: ",sequence)
+sequence[0] = "A"
+print("Mutated sequence at index 0:",sequence)
+sequence.remove("T") #one letter at time, not segment
+print("Mutated sequence without T:",sequence)
+sequence = Seq(sequence) #to convert mutated sequence back to read only object
+
+
+
