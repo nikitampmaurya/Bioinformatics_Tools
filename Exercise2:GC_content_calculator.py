2353bccc478132e3b9d8593d8324e4f5baf2b056
Nikita <132465691+nikitampmaurya@users.noreply.github.com>
2023-10-01 18:40:15 +0530

    Update Exercise2:GC_content_calculator.py

--- Exercise2:GC_content_calculator.py
+++ Exercise2:GC_content_calculator.py
@@ -2,10 +2,22 @@
 Aim: i) To build GC content calculator  
 """
 
-s = "ATGCATGCGCTAgc"
+#while loop is used to check if the given sequence has any number 
+while True:
+    seq = input("I am hungry again, Please feed me a DNA sequence: ")
+    
+    for letter in seq:
+        if letter.isnumeric():
+            print("Oops! your sequence has a number, please try again")
+            break
+            
+    else:
+        print("Thank you for feeding me")
+        break
+
 G_list = []
 C_list = []
-for i in s:
+for i in seq:
     if i == "G" or i=="g":
         G_list +=i
     elif i == "C" or i=="c":
@@ -14,14 +26,14 @@ for i in s:
         pass
 G_count = len(G_list)
 C_count = len(C_list)
-total_count = len(s)
+total_count = len(seq)
 GC_content_calculator = 100*((G_count + C_count)/total_count)
 #print("G list:",G_list)
 #print(C_list)
 #print(G_count)
 #print(C_count)
 #print(total_count)
-print("The GC content in percent is:",round(GC_content_calculator,2)) #to round upto decimal point
+print("The GC content of your sequence in percent:",round(GC_content_calculator,2)) #to round upto decimal point
 
 
 
