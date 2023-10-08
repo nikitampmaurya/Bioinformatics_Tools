46589a75a518523214eca77f2512e6aeeed6c8ea
Nikita <132465691+nikitampmaurya@users.noreply.github.com>
2023-10-08 13:01:03 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,36 @@
+"""
+Aim: Translate mrna into protein
+"""
+s = "AUGCAUAAUUACUGA"  #sequence of mrna
+
+list_of_codon =  {
+    "UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L",
+    "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "UAU": "Y", "UAC": "Y", "UAA": "_", "UAG": "_",
+    "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+    "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K",
+    "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+    "UGA": "_", "UGU": "C", "UGC": "C", "UGG": "W",
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+    "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"
+}
+p = [] #list to store single letter
+for i in range(0,len(s),3):
+    a = s[i:i+3]
+    if a in list_of_codon:
+        if list_of_codon[a] == "_":
+            break
+        else:
+            p += list_of_codon[a]
+protein = "".join(p) #protein
+print("mRNA:",s)
+print("Protein: ",protein)
+
+    
